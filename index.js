20d1f925faca9939f05eb2031b240fc7f61eac67
VOLTA007 <138660852+VOLTA007@users.noreply.github.com>
2024-01-12 07:41:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 $('.hamburger-menu').click(function() {
     $('.menu').toggleClass('show');
     $(this).toggleClass('clicked');
-});
+    $('.b2').toggleClass('show');
+    setTimeout(function() {
+        $('.b2').toggleClass('show');
+      }, 500); 
+    });
